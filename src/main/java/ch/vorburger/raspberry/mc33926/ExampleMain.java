1e01163663829ba609973089f14793d2f1b1604f
adrian-galbenus <adrian-galbenus@gmail.com>
2017-06-04 23:17:10 +0300

    update commit

--- src/main/java/ch/vorburger/raspberry/mc33926/ExampleMain.java
+++ src/main/java/ch/vorburger/raspberry/mc33926/ExampleMain.java
@@ -18,11 +18,13 @@ public class ExampleMain {
 //			triangle(turtle);
 //			dance(turtle);
 //		});
-		twoMotors.motor1().stepTo(300,480);
+		twoMotors.motor2().setSpeed(480);
 		//twoMotors.motor2().setSpeed(480);
 		Thread.sleep(10000);
-		twoMotors.motor1().setSpeed(0);
+		twoMotors.motor2().setSpeed(-480);
+		Thread.sleep(10000);
 		twoMotors.motor2().setSpeed(0);
+
 	}
 
 
